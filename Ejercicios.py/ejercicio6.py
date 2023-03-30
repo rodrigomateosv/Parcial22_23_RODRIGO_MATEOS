8003c362885ee753bbb0c9f555ca214915cfc676
rodrigomateosv <114070557+rodrigomateosv@users.noreply.github.com>
2023-03-30 08:58:36 +0100

    5

--- Ejercicios.py/ejercicio6.py
+++ Ejercicios.py/ejercicio6.py
@@ -1,34 +1,70 @@
-import copy
+class Nodo:
+    def __init__(self, matriz, signo):
+        self.matriz = matriz
+        self.signo = signo
 
-class Matriz:
-    def __init__(self, elementos):
-        self.elementos = elementos
+class Pila:
+    def __init__(self):
+        self.items = []
 
-    def submatriz(self, fila, columna):
-        submatriz = copy.deepcopy(self.elementos)
-        del submatriz[fila]
-        for fila in submatriz:
-            del fila[columna]
-        return Matriz(submatriz)
+    def push(self, item):
+        self.items.append(item)
 
-    def determinante_recursivo(self):
-        if len(self.elementos) == 2:
-            return self.elementos[0][0] * self.elementos[1][1] - self.elementos[0][1] * self.elementos[1][0]
+    def pop(self):
+        return self.items.pop()
+
+    def is_empty(self):
+        return len(self.items) == 0
+
+def submatriz(matriz, col):
+    return [fila[:col] + fila[col+1:] for fila in matriz[1:]]
+
+def determinante_recursivo_con_pila(matriz):
+    pila = Pila()
+    determinante = 0
+
+    for i, coeficiente in enumerate(matriz[0]):
+        pila.push(Nodo(submatriz(matriz, i), ((-1) ** i) * coeficiente))
+
+    while not pila.is_empty():
+        nodo = pila.pop()
+        matriz_actual = nodo.matriz
+        signo_actual = nodo.signo
+
+        if len(matriz_actual) == 1:
+            determinante += signo_actual * matriz_actual[0][0]
         else:
-            det = 0
-            for i in range(len(self.elementos)):
-                coeficiente = self.elementos[0][i]
-                if i % 2 != 0:
-                    coeficiente = -coeficiente
-                det += coeficiente * self.submatriz(0, i).determinante_recursivo()
-            return det
+            for i, coeficiente in enumerate(matriz_actual[0]):
+                pila.push(Nodo(submatriz(matriz_actual, i), signo_actual * ((-1) ** i) * coeficiente))
 
-matriz = Matriz([
-    [2, 3, 1, 4, 5],
-    [3, 1, 4, 2, 6],
-    [5, 3, 2, 4, 7],
-    [6, 1, 5, 2, 8],
-    [7, 4, 3, 5, 9]
-])
+    return determinante
 
-print("Determinante recursivo:", matriz.determinante_recursivo())
+def determinante_iterativo_con_pila(matriz):
+    pila = Pila()
+    determinante = 0
+    signo = 1
+
+    pila.push(Nodo(matriz, 0, 0))
+
+    while not pila.is_empty():
+        nodo = pila.pop()
+        matriz_actual = nodo.matriz
+
+        if len(matriz_actual) == 2:
+            determinante += nodo.signo * (matriz_actual[0][0] * matriz_actual[1][1] - matriz_actual[0][1] * matriz_actual[1][0])
+        else:
+            for i, coeficiente in enumerate(matriz_actual[0]):
+                pila.push(Nodo(submatriz(matriz_actual, i), nodo.signo * ((-1) ** i) * coeficiente))
+
+    return determinante
+
+matriz = [
+    [1, 2, 3, 4, 5],
+    [6, 7, 8, 9, 10],
+    [11, 12, 13, 14, 15],
+    [16, 17, 18, 19, 20],
+    [21, 22, 23, 24, 25]
+]
+
+print("Determinante (recursivo con pila y clases):", determinante_recursivo_con_pila(matriz))
+print("Determinante (iterativo con pila y clases):", determinante_iterativo_con_pila(matriz))
